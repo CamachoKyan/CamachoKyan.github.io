7237df3579e1b9780fdff178327323ae50306655
CamachoKyan <124410885+CamachoKyan@users.noreply.github.com>
2023-06-02 19:31:17 -0700

    Add files via upload

--- Practica5.php
+++ Practica5.php
@@ -4,34 +4,64 @@ $cuadros = $_POST["cuadro"];
 
 $RedFlag = 0;
 
-var_dump($cuadros);
+//var_dump($cuadros);
+
+/*
+[0][1][2]
+[3][4][5]
+[6][7][8]
+*/
 
 //Horizontal
-if($cuadros[0] == $cuadros[1] && $cuadros[2]){
-    echo "Gana " .$cuadros[0];
+if($cuadros[0] == $cuadros[1] && $cuadros[0] == $cuadros[2]){
+    echo "<h1>Gana " .$cuadros[0] ."</h1>";
 }else
 
-if($cuadros[3] == $cuadros[4] && $cuadros[5]){
-    echo "Gana " .$cuadros[3];
+if($cuadros[3] == $cuadros[4] && $cuadros[3] == $cuadros[5]){
+    echo "<h1>Gana " .$cuadros[3] ."</h1>";
 }else
 
-if($cuadros[6] == $cuadros[7] && $cuadros[8]){
-    echo "Gana " .$cuadros[6];
+if($cuadros[6] == $cuadros[7] && $cuadros[6] == $cuadros[8]){
+    echo "<h1>Gana " .$cuadros[6] ."</h1>";
 }else
 //Horizontal
 
+/*
+[0][1][2]
+[3][4][5]
+[6][7][8]
+*/
+
 //Vertical
-if($cuadros[0] == $cuadros[3] && $cuadros[6]){
-    echo "Gana " .$cuadros[0];
+if($cuadros[0] == $cuadros[3] && $cuadros[0] == $cuadros[6]){
+    echo "<h1>Gana " .$cuadros[0] ."</h1>";
 }else
 
-if($cuadros[1] == $cuadros[4] && $cuadros[7]){
-    echo "Gana " .$cuadros[1];
+if($cuadros[1] == $cuadros[4] && $cuadros[1] == $cuadros[7]){
+    echo "<h1>Gana " .$cuadros[1] ."</h1>";
 }else
 
-if($cuadros[2] == $cuadros[5] && $cuadros[8]){
-    echo "Gana " .$cuadros[2];
+if($cuadros[2] == $cuadros[5] && $cuadros[2] == $cuadros[8]){
+    echo "<h1>Gana " .$cuadros[2] ."</h1>";
 }else
 //Vertical
+
+/*
+[0][1][2]
+[3][4][5]
+[6][7][8]
+*/
+
+//Diagonal
+if($cuadros[0] == $cuadros[4] && $cuadros[0] == $cuadros[8]){
+    echo "<h1>Gana " .$cuadros[0] ."</h1>";
+}else
+
+if($cuadros[2] == $cuadros[4] && $cuadros[2] == $cuadros[6]){
+    echo "<h1>Gana " .$cuadros[2] ."</h1>";
+}else{
+    echo "<h1>Empate</h1>";
+}
+//Diagonal
 
 ?>
