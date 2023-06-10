3b22d542aa14084bfb00dc34caee869235006486
CamachoKyan <124410885+CamachoKyan@users.noreply.github.com>
2023-06-09 19:32:31 -0700

    Add files via upload

--- Parcial3/Practica5.php
+++ Parcial3/Practica5.php
@@ -2,65 +2,78 @@
 
 $cuadros = $_POST["cuadro"];
 
-echo "<h1>Resultado Final</h1>";
+//var_dump($cuadros);
 
-// Definir Ganador
+echo "<h1>Resultado Final</h1><h1>";
+
+//Imprimir resultado
+for($i=0;$i<9;$i++){
+    echo "[$cuadros[$i]]";
+
+    if($i==2 || $i==5){
+        echo "<br>";
+    }
+}
+echo "</h1>";
+
+//Definir Ganador
 /*
 [0][1][2]
 [3][4][5]
 [6][7][8]
 */
 
-$cuadroganador = [];
-$Simbolo = [];
+//Horizontal
+if($cuadros[0] == $cuadros[1] && $cuadros[0] == $cuadros[2]){
+    echo "<h1><p style='font-color:green;'>[ a][n ][ a]</p><br>[ ][ ][ ]<br>[ ][ ][ ]</h1>";
+    echo "<h1>Gana " .$cuadros[0] ."</h1>";
+}else
 
-// Comprobar ganador
-// Horizontal
-if ($cuadros[0] == $cuadros[1] && $cuadros[0] == $cuadros[2]) {
-    $cuadroganador = [0, 1, 2];
-} else if ($cuadros[3] == $cuadros[4] && $cuadros[3] == $cuadros[5]) {
-    $cuadroganador = [3, 4, 5];
-} else if ($cuadros[6] == $cuadros[7] && $cuadros[6] == $cuadros[8]) {
-    $cuadroganador = [6, 7, 8];
-}
+if($cuadros[3] == $cuadros[4] && $cuadros[3] == $cuadros[5]){
+    echo "<h1>Gana " .$cuadros[3] ."</h1>";
+}else
 
-// Vertical
-if ($cuadros[0] == $cuadros[3] && $cuadros[0] == $cuadros[6]) {
-    $cuadroganador = [0, 3, 6];
-} else if ($cuadros[1] == $cuadros[4] && $cuadros[1] == $cuadros[7]) {
-    $cuadroganador = [1, 4, 7];
-} else if ($cuadros[2] == $cuadros[5] && $cuadros[2] == $cuadros[8]) {
-    $cuadroganador = [2, 5, 8];
-}
+if($cuadros[6] == $cuadros[7] && $cuadros[6] == $cuadros[8]){
+    echo "<h1>Gana " .$cuadros[6] ."</h1>";
+}else
+//Horizontal
 
-// Diagonal
-if ($cuadros[0] == $cuadros[4] && $cuadros[0] == $cuadros[8]) {
-    $cuadroganador = [0, 4, 8];
-} else if ($cuadros[2] == $cuadros[4] && $cuadros[2] == $cuadros[6]) {
-    $cuadroganador = [2, 4, 6];
-}
+/*
+[0][1][2]
+[3][4][5]
+[6][7][8]
+*/
 
-// Imprimir resultado
-echo "<h1>";
-for ($i = 0; $i < 9; $i++) {
-    $cuadro = $cuadros[$i];
-    if ($cuadroganador && in_array($i,$cuadroganador)) {
-        echo "<span style='color: green;'>[$cuadro]</span>";
-        $Simbolo = $cuadro;
-    } else {
-        echo "[$cuadro]";
-    }
-    if ($i == 2 || $i == 5) {
-        echo "<br>";
-    }
-}
-echo "</h1>";
+//Vertical
+if($cuadros[0] == $cuadros[3] && $cuadros[0] == $cuadros[6]){
+    echo "<h1>Gana " .$cuadros[0] ."</h1>";
+}else
 
-// Imprimir mensaje de ganador o empate
-if (!empty($cuadroganador)) {
-    echo "<h1 style='color: green;'>¡El Ganador Es: $Simbolo!</h1>";
-} else {
+if($cuadros[1] == $cuadros[4] && $cuadros[1] == $cuadros[7]){
+    echo "<h1>Gana " .$cuadros[1] ."</h1>";
+}else
+
+if($cuadros[2] == $cuadros[5] && $cuadros[2] == $cuadros[8]){
+    echo "<h1>Gana " .$cuadros[2] ."</h1>";
+}else
+//Vertical
+
+/*
+[0][1][2]
+[3][4][5]
+[6][7][8]
+*/
+
+//Diagonal
+if($cuadros[0] == $cuadros[4] && $cuadros[0] == $cuadros[8]){
+    echo "<h1>Gana " .$cuadros[0] ."</h1>";
+}else
+
+if($cuadros[2] == $cuadros[4] && $cuadros[2] == $cuadros[6]){
+    echo "<h1>Gana " .$cuadros[2] ."</h1>";
+}else{
     echo "<h1>Empate</h1>";
 }
+//Diagonal
 
 ?>
--- Parcial3/Practica6.php
+++ Parcial3/Practica6.php
@@ -4,31 +4,80 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css">
     <title>Ahorcado</title>
 </head>
 <body>
-    <?php
-    $nombre = $_POST["jugador"]; $palabra = $_POST["palabra"]; $vidas = $_POST["vidas"]; 
-    ?>
-    <div class="container">
-        <div class="row">
+<?php
+       $nombre = $_POST["jugador"]; $palabra = $_POST["palabra"]; $vidas = $_POST["vidas"];
+        $b= false; 
 
-            <h1>Juego Del Ahorcado</h1>
-            <hr>
-            <h3>Nombre Del Jugador: <?php echo $nombre; ?></h3>
-            <br>
-            <h4>Nombre de Vidas: <?php echo $nombre; ?></h4>
-            <br>
-            <?php
-            $letras= str_split($palabra);
-            for ($i=0; $i< (count($letras)); $i++) {
-                echo "<input type='text' name='frase[]' style='width: 5%;'>"; 
+        if(isset($_POST["frase"])){
+            $frase = $_POST["frase"];
+            $letra=$_POST["letra"];
+            //Asigna la letra al array de frase
+            for($i=0 ; $i<count(str_split($palabra));$i++){
+                if($palabra[$i] == $letra){
+                    $frase[$i] = $letra;
+                    $b = true;
+                }
             }
-            ?>
-            <input type="text" name="letra" style="width: 10%">
-            <input type="submit" value="Enviar Letra" class="btn btn-primary" style="width: 10%">
-        </div>
+            //Resta visa en caso de que no coincida la letra
+            if(!$b){
+                echo "<h4>La letra ".$letra." no existe</h4>";
+                $vidas--;
+            }
+            //Valida si de terminaron las vidas
+            if($vidas <= 0){
+                echo "<h3>JaJaJa Que Nuv!</h3><img src='Ja.jpg';><br>";
+                echo "<a href='practica6.html'>Pal Lobby</a>";
+                return;
+            }
+            $terminado = false;
+            //Valida si ganó
+            for($j=0 ; $j<count(str_split($palabra)); $j++){
+                if(!$frase[$j]){
+                    $terminado = true;
+                }
+            }
+
+            if($terminado == false){
+                echo "<h1>Felicidades, ganaste!</h1>";
+                echo "<a href='Practica6.html'>Regresar al inicio</a>";
+                return;
+            }
+
+        }
+    ?>
+     <div class="container">
+        <form action="practica6.php" method="POST">
+            <input type="hidden" value="<?php echo $nombre ?>" name="jugador">
+            <input type="hidden" value="<?php echo $palabra ?>" name="palabra">
+            <input type="hidden" value="<?php echo $vidas ?>" name="vidas">
+
+            <div class="row">
+                <h1>Juego del Ahorcado</h1><hr>
+                <h3>Nombre del jugador: <?php echo $nombre; ?></h3>
+                <h4>Número de vidas: <?php echo $vidas; ?></h4>
+                <?php
+                    $letras = str_split($palabra); //[0]='H', [1]='O', [2]='L', [3]='A';
+                    for($i=0; $i<(count($letras)); $i++){
+                        if(isset($_POST["frase"])){
+                        echo "<input type='text' name='frase[]' style='width: 5%;' value='".$frase[$i]."'>";
+                    }else{
+                        echo "<input type='text' name='frase[]' style='width: 5%;' value=''>";
+                    }}
+                ?>
+                </div>
+                <div class="row mt-4">
+                <div>
+                    <input type="text" name="letra" maxlength="1" required>
+                </div>
+                <div>
+                    <input type="submit" value="Enviar letra" class="btn btn-primary">
+                </div>
+            </div>
+            </form>
     </div>
 </body>
 </html>
